cb945031a172b54200589a49e567c33c017ba91d
Samantha brake <sammy.brake@gmail.com>
2018-02-03 21:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holidayName, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} until ${holiday}!`
 }
